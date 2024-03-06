3bb01e8d747c3ea42366d3b50eecb31559c6e8ee
unimag-user <unimag@unimagdalena.edu.co>
2024-03-05 19:00:09 -0500

    user repository

--- 01-Task/src/main/java/com/reminder/memo/repository/UserRepository.java
+++ 01-Task/src/main/java/com/reminder/memo/repository/UserRepository.java
@@ -1,9 +1,14 @@
 package com.reminder.memo.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import com.reminder.memo.model.User;
+import java.util.List;
 
 public interface UserRepository extends JpaRepository<User, Long> {
+    public List<User> findByUsername(String username);
 
+    @Query("SELECT u FROM Users u WHERE u.name = ?1 AND u.lastname = ?2")
+    public List<User> findByNameAndLastname(String name, String lastname);
 }
