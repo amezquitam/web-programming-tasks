22dc52bc0d28c91ef9ea7022921593bb86d4cbe1
amezquitam <amezquitamiguel7@gmail.com>
2024-03-13 05:38:08 -0500

    base project

---
+++ src/main/java/com/uraraka/socialz/SocialzApplication.java
@@ -0,0 +1,13 @@
+package com.uraraka.socialz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SocialzApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SocialzApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/uraraka/socialz/SocialzApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uraraka.socialz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SocialzApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
