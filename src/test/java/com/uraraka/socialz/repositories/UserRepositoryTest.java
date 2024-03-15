121d60b92113fab9a5d807ca066eb47ae2115c89
amezquitam <amezquitamiguel7@gmail.com>
2024-03-14 20:35:38 -0500

    UserRepositoryTest testing insertion of a user

---
+++ src/test/java/com/uraraka/socialz/repositories/UserRepositoryTest.java
@@ -0,0 +1,30 @@
+package com.uraraka.socialz.repositories;
+
+import com.uraraka.socialz.AbstractIntegrationDBTest;
+import com.uraraka.socialz.entities.User;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+
+public class UserRepositoryTest extends AbstractIntegrationDBTest {
+    UserRepository userRepository;
+
+    @Autowired
+    public UserRepositoryTest(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Test
+    public void whenInsertAUser_thenUserIdIsNotNull() {
+        User user = User.builder()
+                .username("Mario12")
+                .firstname("Mario")
+                .lastname("Méndez")
+                .build();
+
+        User userSaved = userRepository.save(user);
+
+        Assertions.assertThat(userSaved.getUserId()).isNotNull();
+    }
+}
