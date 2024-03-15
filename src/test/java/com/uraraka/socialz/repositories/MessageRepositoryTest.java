e81cdaf2f2018b7cc9e5fa007c78ccfbd1494f97
amezquitam <amezquitamiguel7@gmail.com>
2024-03-15 12:06:39 -0500

    Adding unit tests to MatchRepository,MessageRepository and SuggestionRepository.

---
+++ src/test/java/com/uraraka/socialz/repositories/MessageRepositoryTest.java
@@ -0,0 +1,13 @@
+package com.uraraka.socialz.repositories;
+
+import com.uraraka.socialz.AbstractIntegrationDBTest;
+import org.junit.jupiter.api.BeforeEach;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MessageRepositoryTest extends AbstractIntegrationDBTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+}
---
+++ src/test/java/com/uraraka/socialz/repositories/SuggestionRepositoryTest.java
@@ -0,0 +1,12 @@
+package com.uraraka.socialz.repositories;
+
+import com.uraraka.socialz.AbstractIntegrationDBTest;
+import org.junit.jupiter.api.BeforeEach;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SuggestionRepositoryTest extends AbstractIntegrationDBTest {
+    @BeforeEach
+    void setUp() {
+    }
+}
