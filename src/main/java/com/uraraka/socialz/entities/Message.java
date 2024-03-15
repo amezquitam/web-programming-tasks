830c95ea43bc3d949a8348838f52a7ada43cddbe
amezquitam <amezquitamiguel7@gmail.com>
2024-03-14 19:29:42 -0500

    Adding Builder Pattern to entities with Lombok

--- src/main/java/com/uraraka/socialz/entities/Message.java
+++ src/main/java/com/uraraka/socialz/entities/Message.java
@@ -1,10 +1,7 @@
 package com.uraraka.socialz.entities;
 
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 
 import java.time.LocalDateTime;
 
@@ -14,6 +11,7 @@ import java.time.LocalDateTime;
 @AllArgsConstructor
 @Getter
 @Setter
+@Builder
 public class Message {
 
     @Id
